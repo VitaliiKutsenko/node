8bc7e0a3f18b07586d17fcb6d562f2e3a628ba70
vitalii89 <vitalii.kutsenko89@gmail.com>
2022-08-30 15:14:23 +0300

    lesson_3 import/export

---
+++ lesson_3/lesson_3_export.js
@@ -0,0 +1,10 @@
+// import/export
+
+const userName = (name) => {
+    return `Hello my name is ${name}`
+}
+const user = 'Vitalii'
+module.exports = {
+    userName,
+    user
+}
---
+++ lesson_3/lesson_3_import.js
@@ -0,0 +1,5 @@
+// import/export
+const os = require('os')
+const {userName,user} = require('./lesson_3_export')
+console.log(userName(user))
+console.log(os.release(),os.version())
