3555bb2ab59bbd600b412d4374e8b890ec022853
vitalii89 <vitalii.kutsenko89@gmail.com>
2022-09-03 12:04:02 +0300

    lesson_9 add express & ejs, refactor previous code

--- lesson_8/app.js
+++ lesson_8/app.js
@@ -7,39 +7,39 @@ const createPath = (page) => path.resolve(__dirname, 'views', `${page}.html`)
 
 const port = 3000
 const server = http.createServer((req, res) => {
-    res.setHeader('Content-Type', 'text/html')
-    let page
-    switch (req.url) {
-        case '/' :
-            page = createPath('index')
-            res.statusCode = 200
-            break
-        case '/about-us' :
-            res.statusCode = 301
-            res.setHeader('Location', '/contacts')
-            res.end()
-            break
-        case '/contacts' :
-            page = createPath('contacts')
-            res.statusCode = 200
-            break
-        default :
-            page = createPath('error')
-            res.statusCode = 404
-    }
-
-    fs.readFile(page, (err, data) => {
-        if (err) {
-            console.log(err)
-            res.statusCode = 500
-            res.end()
-        } else {
-            res.write(data)
-            res.end()
+        res.setHeader('Content-Type', 'text/html')
+        let page
+        switch (req.url) {
+            case '/' :
+                page = createPath('index')
+                res.statusCode = 200
+                break
+            case '/about-us' :
+                res.statusCode = 301
+                res.setHeader('Location', '/contacts')
+                res.end()
+                break
+            case '/contacts' :
+                page = createPath('contacts')
+                res.statusCode = 200
+                break
+            default :
+                page = createPath('error')
+                res.statusCode = 404
         }
-    })
-})
 
-server.listen(port, 'localhost', err => {
-    err ? console.log(err) : console.log(`listen port ${port}`)
-})
+        fs.readFile(page, (err, data) => {
+            if (err) {
+                console.log(err)
+                res.statusCode = 500
+                res.end()
+            } else {
+                res.write(data)
+                res.end()
+            }
+        })
+    })
+    < h1 >
+    server.listen(port, 'localhost', err => {
+        err ? console.log(err) : console.log(`listen port ${port}`)
+    })
